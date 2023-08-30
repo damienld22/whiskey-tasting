12553acf5b4f3778778eb21289d4455bde3bd2c3
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2023-08-30 22:49:07 +0200

    chore: Add empty Spring boot project

---
+++ whiskey-tasting-backend/src/main/java/com/damienld22/whiskeytastingbackend/WhiskeyTastingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.damienld22.whiskeytastingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeyTastingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskeyTastingBackendApplication.class, args);
+	}
+
+}
---
+++ whiskey-tasting-backend/src/test/java/com/damienld22/whiskeytastingbackend/WhiskeyTastingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damienld22.whiskeytastingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskeyTastingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
