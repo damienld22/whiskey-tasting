2b727dec491c87bc8ae70778353181fa3752a018
ledantec-d_ext <ledantec-d_ext@mgdis.fr>
2023-09-26 13:48:53 +0200

    feat: Add button

--- whiskey-tasting-app/src/App.vue
+++ whiskey-tasting-app/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import AddButton from './components/AddButton.vue'
+
 const f7Params = {
   name: 'Whiskey tasting'
 }
@@ -7,6 +9,8 @@ const f7Params = {
 <template>
   <f7-app v-bind="f7Params">
     <f7-navbar :title="f7Params.name"></f7-navbar>
+
+    <AddButton />
   </f7-app>
 </template>
 
---
+++ whiskey-tasting-app/src/components/AddButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <f7-fab position="right-bottom">
+    <f7-icon ios="f7:plus" md="material:add"></f7-icon>
+  </f7-fab>
+</template>
